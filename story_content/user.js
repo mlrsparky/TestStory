f498d6e96f4da879825d10c3efb88eb2fb27e790
mlrsparky <91495947+mlrsparky@users.noreply.github.com>
2022-04-03 18:31:10 -0400

    Update user.js

--- story_content/user.js
+++ story_content/user.js
@@ -1,30 +1,25 @@
-function ExecuteScript(strId)
-{
-  switch (strId)
-  {
-      case "688Ax3DAspO":
-        Script1();
-        break;
-      case "5vKn3gIZwpW":
-        Script2();
-        break;
+function ExecuteScript(strId) {
+  switch (strId) {
+    case "688Ax3DAspO":
+      Script1();
+      break;
+    case "5vKn3gIZwpW":
+      Script2();
+      break;
   }
 }
 
-function Script1()
-{
+function Script1() {
   var allAudios = document.getElementsByTagName("audio");
-for (var i = 0; i < allAudios.length; i++) {
-   allAudios[i].playbackRate = 2.0;
+  console.log(allAudios);
+  for (var i = 0; i < allAudios.length; i++) {
+    allAudios[i].playbackRate = 2.0;
+  }
 }
 
-}
-
-function Script2()
-{
+function Script2() {
   var allAudios = document.getElementsByTagName("audio");
-for (var i = 0; i < allAudios.length; i++) {
-   allAudios[i].playbackRate = 0.5;
+  for (var i = 0; i < allAudios.length; i++) {
+    allAudios[i].playbackRate = 0.5;
+  }
 }
-}
-
